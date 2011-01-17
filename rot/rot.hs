beb4310255f31833faec48088620320eed974f8c
Pete Morris <pete.morris@gmail.com>
2011-01-17 17:06:41 -0500

    Start of handling input from stdin in haskell.

--- rot/rot.hs
+++ rot/rot.hs
@@ -1,5 +1,6 @@
 import System
 import System.IO
+import IO
 import Char
 
 usage :: IO ()
@@ -18,11 +19,27 @@ main = do
 handleArgs :: [String] -> IO (Int, String)
 handleArgs args = do
     case length args of
+        0 -> do
+             words' <- readWords
+             handleArgs words'
+
         2 -> return (read a :: Int, b)
              where a : b : _ = args
+
         _ -> do
              usage
              exitWith (ExitFailure 1)
+
+readWords :: IO [String]
+readWords = do
+    line <- getLine `catch` eofHandler
+    return $ words line
+
+eofHandler :: IOError -> IO a
+eofHandler e = 
+    if isEOFError e
+        then exitWith ExitSuccess
+        else ioError e
 
 
 rotate :: Int -> String -> String
