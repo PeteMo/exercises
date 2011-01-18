7ba61dd4a9d3c992b9e801d327d4e0c78c82e9e8
Pete Morris <pete.morris@gmail.com>
2011-01-18 12:37:23 -0500

    Start branch for modifying code to read from stdin.

--- rot/rot.hs
+++ rot/rot.hs
@@ -1,5 +1,6 @@
 import System
 import System.IO
+import IO
 import Char
 
 usage :: IO ()
@@ -7,11 +8,9 @@ usage = do
     progName <- getProgName
     hPutStrLn stderr $ "Usage: " ++ progName ++ " factor string"
 
-
 rotate :: Int -> String -> String
 rotate factor string = 
     [rotateChar factor c | c <- string]
-
 
 rotateChar :: Int -> Char -> Char
 rotateChar factor c 
@@ -20,20 +19,34 @@ rotateChar factor c
     where offset | isUpper c = ord 'A'
                  | otherwise = ord 'a'
 
-
 main :: IO ()
 main = do
     args <- getArgs
     (factor, string) <- handleArgs args
     putStrLn $ rotate factor string
 
-
+-- Takes a list of strings. Returns a pair where the first string
+-- is an Int and the rest of the list is a string.
 handleArgs :: [String] -> IO (Int, String)
 handleArgs args = do
     case length args of
+        0 -> do
+             words' <- readWords
+             handleArgs words'
+
         2 -> return (read a :: Int, b)
              where a : b : _ = args
 
         _ -> do
              usage
              exitWith (ExitFailure 1)
+
+-- Read from stdin and return a list of two strings; the first word and the
+-- rest of the words combined into one string.
+readWords :: IO [String]
+readWords = do
+    line <- getLine `catch` eofHandler
+    let a : b = words line
+    return $ [a, unwords b]
+    where eofHandler e | isEOFError e = exitWith ExitSuccess
+                       | otherwise    = ioError e
