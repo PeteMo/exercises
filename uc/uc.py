d67828bf854f39ec363499444eedb4712676a4c4
Pete Morris <pete.morris@gmail.com>
2011-01-19 20:40:10 -0500

    Process multiple command line arguments in the python version.

--- uc/uc.py
+++ uc/uc.py
@@ -3,15 +3,16 @@
 import sys, os.path
 
 def main(argv = sys.argv):
+    # Read from stdin if there aren't any command line arguments.
     if len(argv) == 1:
         string = sys.stdin.readline()
         while (string):
             print string.upper().strip()
             string = sys.stdin.readline()
 
+    # Handle command line arguments.
     else:
-        string = argv[1]
-        print string.upper()
+        print " ".join([s.upper() for s in argv[1:]])
 
 
 if __name__ == "__main__":
