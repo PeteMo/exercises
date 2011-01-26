6a1bedf3ca4a4c6088efdde0e9e11d0fff998c6f
Pete Morris <pete.morris@gmail.com>
2011-01-26 14:42:19 -0500

    Code cleanup

--- guess/guess.hs
+++ guess/guess.hs
@@ -7,11 +7,12 @@ getRandom low high = do
     let (r,_) = randomR (low, high) gen :: (Int, StdGen)
     return r
 
+
 main :: IO ()
 main = do
     let numGuesses = 3
     r <- getRandom 1 10
-    putStrLn $ "Guess my number between 1 and 10"
+    putStrLn $ "You have " ++ show numGuesses ++ " tries to guess my number between 1 and 10."
     play r numGuesses
 
 
@@ -19,19 +20,15 @@ play :: Int -> Int -> IO ()
 play r numGuesses = do
     case numGuesses of
         0 -> putStrLn ("You lose, the number was " ++ show r) >> exitWith ExitSuccess
-        1 -> putStrLn ( "You have " ++ show numGuesses ++ " guess remaining. What is your guess?") >> evalGuess r numGuesses
-        _ -> putStrLn ( "You have " ++ show numGuesses ++ " guesses remaining. What is your guess?") >> evalGuess r numGuesses
-
-
-evalGuess :: Int -> Int -> IO ()
-evalGuess r n = do
-    guess <- getLine
-    case checkGuess (read guess :: Int) r of
-        0  -> putStrLn "You Win!" >> exitWith ExitSuccess
-        1  -> putStrLn "Too High"
-        -1 -> putStrLn "Too Low"
-        _  -> error "Unpossible!"
-    play r (n-1)
+        _ -> do
+            putStrLn $ "What is your guess?"
+            guess <- getLine
+            case checkGuess (read guess :: Int) r of
+                0  -> putStrLn "You Win!" >> exitWith ExitSuccess
+                1  -> putStrLn "Too High."
+                -1 -> putStrLn "Too Low."
+                _  -> error "Unpossible!"
+            play r (numGuesses - 1)
 
 
 checkGuess :: Int -> Int -> Int
