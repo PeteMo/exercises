180231c0ba574cff3cc6ee121033d36a9b6c2def
Pete Morris <pete.morris@gmail.com>
2011-01-26 20:02:44 -0500

    Add some flexibility.

--- guess/guess.hs
+++ guess/guess.hs
@@ -11,23 +11,26 @@ getRandom low high = do
 main :: IO ()
 main = do
     let numGuesses = 3
-    r <- getRandom 1 10
-    putStrLn $ "You have " ++ show numGuesses ++ " tries to guess my number between 1 and 10."
+    let (low, high) = (1, 10)
+    r <- getRandom low high
+    putStrLn $ "You have " ++ show numGuesses ++ " tries to guess my number between " ++ show low ++ " and " ++ show high ++ "."
     play r numGuesses
 
 
 play :: Int -> Int -> IO ()
 play r numGuesses = do
     case numGuesses of
-        0 -> putStrLn ("You lose, the number was " ++ show r) >> exitWith ExitSuccess
+        0 -> putStrLn ("You lose, the number was " ++ show r ++ ".") >> exitWith ExitSuccess
         _ -> do
             putStrLn $ "What is your guess?"
             guess <- getLine
+
             case checkGuess (read guess :: Int) r of
                 0  -> putStrLn "You Win!" >> exitWith ExitSuccess
                 1  -> putStrLn "Too High."
                 -1 -> putStrLn "Too Low."
                 _  -> error "Unpossible!"
+
             play r (numGuesses - 1)
 
 
